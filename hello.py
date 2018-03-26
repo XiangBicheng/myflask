edbba2fa40dd93ab5e3c0791e895871cbe824bd1
Bicheng Xiang <xiangbcyx@qq.com>
2018-03-26 14:52:44 +0800

    localize date and time

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from datetime import datetime
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html', current_time=datetime.utcnow()) 
 
 @app.route('/user/<name>')
 def user(name):
