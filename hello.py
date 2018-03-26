b5ffc1fa1c1d063cf5de285ccf416ab7ee7b94b1
Bicheng Xiang <xiangbcyx@qq.com>
2018-03-26 21:52:53 +0800

    Post/Redirect/Get

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -18,13 +18,12 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     #check whether it's GET or POST request
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name) 
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name')) 
 
 @app.route('/user/<name>')
 def user(name):
