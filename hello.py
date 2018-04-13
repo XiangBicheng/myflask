6faf126fab0ccfa60e54ca76a5c88710c21b1e46
Bicheng Xiang <xiangbcyx@qq.com>
2018-04-13 20:37:19 +0800

    adding a shell context

--- hello.py
+++ hello.py
@@ -51,14 +51,19 @@ class User(db.Model):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    #check whether it's GET or POST request
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name')) 
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'), known=session.get('known', False)) 
 
 @app.route('/user/<name>')
 def user(name):
@@ -71,3 +76,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
